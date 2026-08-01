8b85be45bb4762600ab246eddef23b01a00a813a
Ivan Adithya <ivanjayasooriya03@gmail.com>
2026-08-01 08:32:23 +0530

    Add validation annotations to User model and include spring-boot-starter-validation dependency in Maven

--- user-service/src/main/java/lk/ijse/userservice/model/User.java
+++ user-service/src/main/java/lk/ijse/userservice/model/User.java
@@ -1,8 +1,13 @@
 package lk.ijse.userservice.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,15 +15,22 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 public class User {
     private String id;
 
+    @NotBlank(message = "Name is Required")
     private String name;
 
+    @NotBlank(message = "Email is Required")
+    @Email(message = "Email must be Valid")
     private String email;
 
+    @NotBlank(message = "Password is Required")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
+    @NotNull(message = "Role is Required")
     private UserRole role;
 
     private List<String> bookingHistory = new ArrayList<>();
